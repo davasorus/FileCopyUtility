5847cc01a03621dcbb7cf8bb95c0d1fe221c3821
davasorus <davitts2@gmail.com>
2020-01-04 13:04:02 -0500

    Updated for Youtube video work flow

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Help_Button = new System.Windows.Forms.Button();
+            this.Copy_Button = new System.Windows.Forms.Button();
+            this.Location_Copy_From = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Location_Copy_To = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Async_File_Copy = new System.Windows.Forms.CheckBox();
+            this.Normal_File_Copy = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Location_ZipFIle_To = new System.Windows.Forms.TextBox();
+            this.CompressedFileName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.TSL = new System.Windows.Forms.ToolStripLabel();
+            this.TSProgBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Help_Button
+            // 
+            this.Help_Button.Location = new System.Drawing.Point(292, 13);
+            this.Help_Button.Name = "Help_Button";
+            this.Help_Button.Size = new System.Drawing.Size(75, 23);
+            this.Help_Button.TabIndex = 0;
+            this.Help_Button.Text = "Help";
+            this.Help_Button.UseVisualStyleBackColor = true;
+            this.Help_Button.Click += new System.EventHandler(this.Help_Button_Click);
+            // 
+            // Copy_Button
+            // 
+            this.Copy_Button.Location = new System.Drawing.Point(292, 185);
+            this.Copy_Button.Name = "Copy_Button";
+            this.Copy_Button.Size = new System.Drawing.Size(75, 23);
+            this.Copy_Button.TabIndex = 5;
+            this.Copy_Button.Text = "Copy";
+            this.Copy_Button.UseVisualStyleBackColor = true;
+            this.Copy_Button.Click += new System.EventHandler(this.Copy_Button_Click);
+            // 
+            // Location_Copy_From
+            // 
+            this.Location_Copy_From.Location = new System.Drawing.Point(1, 68);
+            this.Location_Copy_From.Name = "Location_Copy_From";
+            this.Location_Copy_From.Size = new System.Drawing.Size(360, 20);
+            this.Location_Copy_From.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Location you are copy from";
+            // 
+            // Location_Copy_To
+            // 
+            this.Location_Copy_To.Location = new System.Drawing.Point(1, 107);
+            this.Location_Copy_To.Name = "Location_Copy_To";
+            this.Location_Copy_To.Size = new System.Drawing.Size(360, 20);
+            this.Location_Copy_To.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Location you are copying to";
+            // 
+            // Async_File_Copy
+            // 
+            this.Async_File_Copy.AutoSize = true;
+            this.Async_File_Copy.Location = new System.Drawing.Point(13, 13);
+            this.Async_File_Copy.Name = "Async_File_Copy";
+            this.Async_File_Copy.Size = new System.Drawing.Size(101, 17);
+            this.Async_File_Copy.TabIndex = 100;
+            this.Async_File_Copy.Text = "Async File Copy";
+            this.Async_File_Copy.UseVisualStyleBackColor = true;
+            // 
+            // Normal_File_Copy
+            // 
+            this.Normal_File_Copy.AutoSize = true;
+            this.Normal_File_Copy.Location = new System.Drawing.Point(146, 13);
+            this.Normal_File_Copy.Name = "Normal_File_Copy";
+            this.Normal_File_Copy.Size = new System.Drawing.Size(106, 17);
+            this.Normal_File_Copy.TabIndex = 101;
+            this.Normal_File_Copy.Text = "Normal FIle Copy";
+            this.Normal_File_Copy.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-2, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Compression Folder";
+            // 
+            // Location_ZipFIle_To
+            // 
+            this.Location_ZipFIle_To.Location = new System.Drawing.Point(1, 185);
+            this.Location_ZipFIle_To.Name = "Location_ZipFIle_To";
+            this.Location_ZipFIle_To.Size = new System.Drawing.Size(134, 20);
+            this.Location_ZipFIle_To.TabIndex = 4;
+            // 
+            // CompressedFileName
+            // 
+            this.CompressedFileName.Location = new System.Drawing.Point(1, 146);
+            this.CompressedFileName.Name = "CompressedFileName";
+            this.CompressedFileName.Size = new System.Drawing.Size(360, 20);
+            this.CompressedFileName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Name of Compressed File";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSL,
+            this.TSProgBar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 211);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(373, 25);
+            this.toolStrip1.TabIndex = 12;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // TSL
+            // 
+            this.TSL.Name = "TSL";
+            this.TSL.Size = new System.Drawing.Size(0, 22);
+            // 
+            // TSProgBar
+            // 
+            this.TSProgBar.Name = "TSProgBar";
+            this.TSProgBar.Size = new System.Drawing.Size(100, 22);
+            this.TSProgBar.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 236);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CompressedFileName);
+            this.Controls.Add(this.Location_ZipFIle_To);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Normal_File_Copy);
+            this.Controls.Add(this.Async_File_Copy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Location_Copy_To);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Location_Copy_From);
+            this.Controls.Add(this.Copy_Button);
+            this.Controls.Add(this.Help_Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Help_Button;
+        private System.Windows.Forms.Button Copy_Button;
+        private System.Windows.Forms.TextBox Location_Copy_From;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Location_Copy_To;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox Async_File_Copy;
+        private System.Windows.Forms.CheckBox Normal_File_Copy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Location_ZipFIle_To;
+        private System.Windows.Forms.TextBox CompressedFileName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel TSL;
+        private System.Windows.Forms.ToolStripProgressBar TSProgBar;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApp1
+{
+    /*
+    Created 02/15/2018
+    Created by Sean Davitt, Tyler Technologies NWPS Mobile Implimentation Specialist
+    Designed for 64bit and 32bit windows
+
+        WIP
+            Faster execution without sacrificing accuracy
+            Message boxes that display text
+                1. When no operation is selected
+                2. When common errors occur
+                3. Others I'm sure
+            Speed testing between the two copy methods
+
+        WORK FLOW FOR PROGRAM:
+            Location you want to copy from (user defined) => compression to a backup folder(user defined)
+
+            => zip folder is copied to the location you want to copy to(user defined)
+
+            => Decompressed in the location you want to copy files to BUT in now in the "Extracted files" folder
+    */
+
+    public partial class Form1 : Form
+    {
+        private XmlDocument StartupSettings = new XmlDocument();
+        private string SourcePath = @"";
+        private string TargetPath = @"";
+        private string BackUpPath = @"";
+        private string CompressionFileLable = "";
+        public string ClientPath1 { get => TargetPath; set => TargetPath = value; }
+        public string SourcePath1 { get => SourcePath; set => SourcePath = value; }
+
+        //What the form does with it initializes every time
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitialLoadofXML();
+
+            TSL.Text = "Ready to Copy files";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bg = new BackgroundWorker();
+            bg.DoWork += Bg_DoWork;
+            //bg.ProgressChanged += Bg_ProgressChanged;
+            bg.RunWorkerCompleted += Bg_RunWorkerCompleted;
+            bg.WorkerReportsProgress = true;
+        }
+
+        //Copy button
+        private void Copy_Button_Click(object sender, EventArgs e)
+        {
+            bg.RunWorkerAsync();
+        }
+
+        //Copies all directories and subfolders
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        //Copies all files from one location to another asynchronously
+        //The added "speed" might result in data corruption
+        //"speed" - less waiting on I/O so the OS will move whatever information it has at all times
+        //so that it is a genuine constant stream of data
+        public async Task AsyncCopy()
+        {
+            string StartDir = Location_ZipFIle_To.Text;
+            string EndDir = Location_Copy_To.Text;
+
+            TSL.Text = "Copying from Compression Location to Save location.";
+
+            try
+            {
+                foreach (string dirPath in Directory.GetDirectories(StartDir, "*", SearchOption.AllDirectories))
+                {
+                    Directory.CreateDirectory(dirPath.Replace(StartDir, EndDir));
+                    foreach (string filename in Directory.EnumerateFiles(dirPath))
+                    {
+                        using (FileStream SourceStream = File.Open(filename, FileMode.Open))
+                        {
+                            using (FileStream DestinationStream = File.Create(filename.Replace(StartDir, EndDir)))
+                            {
+                                await SourceStream.CopyToAsync(DestinationStream);
+                            }
+                        }
+                    }
+                }
+
+                foreach (string filename in Directory.EnumerateFiles(StartDir))
+                {
+                    using (FileStream SourceStream = File.Open(filename, FileMode.Open))
+                    {
+                        using (FileStream DestinationStream = File.Create(EndDir + filename.Substring(filename.LastIndexOf('\\'))))
+                        {
+                            await SourceStream.CopyToAsync(DestinationStream);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());
+            }
+        }
+
+        //Compresses all files/folders from a user defined location to a "back up" folder
+        //The end folder for compression CAN NOT be the same location the files are coming from.
+        private void Compress()
+        {
+            TSL.Text = "Compressing Folder";
+            string StartPath = Location_Copy_From.Text;
+            string ZipPath = Location_ZipFIle_To.Text;
+            System.IO.Compression.ZipFile.CreateFromDirectory(StartPath, ZipPath + @"\" + CompressedFileName.Text + ".zip");
+        }
+
+        //XML related information. Broken up between loading prior XML or creating a new XML with places holder information
+        private void InitialLoadofXML()
+        {
+            //Checking if the CopyFiles.xml exists, and loading the data if it does.
+            if (File.Exists("CopyFiles.xml"))
+            {
+                StartupSettings.Load("CopyFiles.xml");
+
+                Location_Copy_From.Text = StartupSettings.GetElementsByTagName("SourcePath")[0].InnerText;
+                Location_Copy_To.Text = StartupSettings.GetElementsByTagName("TargetPath")[0].InnerText;
+                Location_ZipFIle_To.Text = StartupSettings.GetElementsByTagName("BackUpPath")[0].InnerText;
+                CompressedFileName.Text = StartupSettings.GetElementsByTagName("CompressionFileLable")[0].InnerText;
+            }
+
+            //Creation of a new CopyFiles.xml if one does not already exist.
+            else
+            {
+                SourcePath = Location_Copy_From.Text;
+                TargetPath = Location_Copy_To.Text;
+                BackUpPath = Location_ZipFIle_To.Text;
+                CompressionFileLable = CompressedFileName.Text;
+
+                //root of the XML
+                XmlNode root = StartupSettings.CreateElement("root");
+                StartupSettings.AppendChild(root);
+
+                //COPY FROM PATH
+                XmlNode Location_Copy_FromPathNode = StartupSettings.CreateElement("SourcePath");
+                Location_Copy_FromPathNode.InnerText = @"Copy from location";
+                root.AppendChild(Location_Copy_FromPathNode);
+
+                //COPY TO PATH
+                XmlNode Location_Copy_ToPathNode = StartupSettings.CreateElement("TargetPath");
+                Location_Copy_ToPathNode.InnerText = @"Copy to location";
+                root.AppendChild(Location_Copy_ToPathNode);
+
+                ///COPY TO PATH
+                XmlNode Location_ZipFile_ToPathNode = StartupSettings.CreateElement("BackUpPath");
+                Location_ZipFile_ToPathNode.InnerText = @"C:\Temp";
+                root.AppendChild(Location_ZipFile_ToPathNode);
+
+                XmlNode CompressionPathNode = StartupSettings.CreateElement("CompressionFileLable");
+                CompressionPathNode.InnerText = "Back Up";
+                root.AppendChild(CompressionPathNode);
+
+                //Save the start up settings
+                StartupSettings.Save("CopyFiles.xml");
+            }
+        }
+
+        //WIP so that when the placeholder text is modified the XML maintains the modification for the subsiquint uses.
+        private void SaveStartupSettings()
+        {
+            StartupSettings.GetElementsByTagName("SourcePath")[0].InnerText = Location_Copy_From.Text;
+            StartupSettings.GetElementsByTagName("TargetPath")[0].InnerText = Location_Copy_To.Text;
+            StartupSettings.Save("CopyFiles.xml");
+        }
+
+        //This will create the back up location
+        //WIP - will delete BackUp.zip if it is already present
+        private void Temp()
+        {
+            Directory.CreateDirectory(Location_ZipFIle_To.Text);
+        }
+
+        //Will display a message when the Compression-Copy-Decompression is finished
+        //WIP clean download and decompression
+        //WIP give option to rerun the program for other files
+        private void MessageCopy()
+        {
+            MessageBox.Show("Copy is complete");
+            TSL.Text = "Copy is Complete. Close Program or Copy a new folder.";
+        }
+
+        //This will clean up all files that are not wanted on the new local system.
+        //Contant WIP as I get new information to delete
+        private void CleanUp()
+        {
+            TSL.Text = "Deleting Back Up";
+
+            //This will delete the zipped folder on the new machine
+            string[] directoryFiles = Directory.GetFiles(Location_ZipFIle_To.Text, "*.zip");
+
+            foreach (string directoryFile in directoryFiles)
+            {
+                File.Delete(directoryFile);
+            }
+        }
+
+        //Help Button
+        private void Help_Button_Click(object sender, EventArgs e)
+        {
+            var Message = " Help Button .\n\n";
+            Message += "1. Async Copy \n\n";
+            Message += "    1a. Will copy files in a continuous stream of information (there is very little I/O downtime as each file is accessed at one time.) \n\n";
+            Message += "2. Normal Copy Copy \n\n";
+            Message += "    2a. Will copy files one at a time (there is a little bit of I/O lead time as it accesses each file at a time.) \n\n";
+            Message += "3. Location you are copying from \n\n";
+            Message += "    3a. Location you are copying all files from. \n\n";
+            Message += "4. Location you are copying to. \n\n";
+            Message += "    4a. Location you want all files you are copying to end up. \n\n";
+            Message += "5. Back up Location \n\n";
+            Message += "    5a. Local location you want the ZIP file to be. \n\n";
+            Message += "6. If you have any issues or questions please call Sean Davitt. \n\n";
+            Message += "    6a. Please call me at 609 709 5449. \n\n";
+            Message += "    6a. Please email me at sean.davitt@tylertech.com. \n\n";
+
+            MessageBox.Show(Message);
+        }
+
+        private BackgroundWorker bg;
+
+        private void Bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Task completed");
+        }
+
+        private void Bg_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (Async_File_Copy.Checked == true)
+            {
+                SaveStartupSettings();
+
+                //See Temp Below
+                Temp();
+                //See compress below
+                Compress();
+
+                //Runs the AsyncCopy function as a task to ensure it completes
+                //and does not allow decompress(); to access a file that needs to be copied
+                var t = Task.Run(() => AsyncCopy());
+                t.Wait();
+
+                //See CleanUp below
+                CleanUp();
+
+                //Displays a message and closes the application
+                MessageCopy();
+            }
+
+            //Code to execute if someone wants to copy files synchronously(Normally)
+            if (Normal_File_Copy.Checked == true)
+            {
+                //See Temp Below
+                Temp();
+
+                //''
+                Compress();
+
+                //Copies all directories and files from one location to another
+                //this specifically means one zip folder from one location to another and will over-right a file if need be.
+                DirectoryCopy(Location_ZipFIle_To.Text, Location_Copy_To.Text, true);
+
+                //''
+                CleanUp();
+
+                //''
+                MessageCopy();
+            }
+        }
+    }
+}
